71ba9b95c2d27a44c6bf5b41436ed53c4de0bed2
wangt47 <60717538+wangt47@users.noreply.github.com>
2020-02-05 15:26:16 -0800

    Add project files.

---
+++ BankAccounts/Account.cs
@@ -0,0 +1,106 @@
+﻿/*
+ CISP 405 Summer 2019 Tony Wang SID # 1443145
+ 
+ Assignment 2 Description:
+ Modify two given C# files (Account.cs and AccountTest.cs)
+ for learning how to code in C# language.
+ 
+
+ Account.cs
+ Making the framework of creating a Bank Account and
+ providing methods to allow deposit and withdrawal
+ but only accepts a valid value for the appropriate function
+ (i.e. cannot withdraw if value is over the account balance, 
+ can deposit if value is a positive number)
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    class Account
+    {
+        public string Name { get; set; } // auto-implemented property
+        private decimal balance; // instance variable
+
+        // Account constructor that receives two parameters  
+        public Account(string accountName, decimal initialBalance)
+        {
+            Name = accountName;
+            Balance = initialBalance; // Balance's set accessor validates
+        }
+
+        // Balance property with validation
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            private set // can be used only within the class
+            {
+                // validate that the balance is greater than 0.0; if it's not,
+                // instance variable balance keeps its prior value
+                if (value > 0.0m) // m indicates that 0.0 is a decimal literal
+                {
+                    balance = value;
+                }
+
+                // validate that the balance is 0; if it is,
+                // instance variable balance is set to zero.
+                else if (value == 0.0m)
+                {
+                    balance = 0;
+                }
+            }
+        }
+
+        // method that deposits (adds) only a valid amount to the balance
+        public void Deposit(decimal depositAmount)
+        {
+            if (depositAmount > 0.0m) // if the depositAmount is valid
+            {
+                Balance = Balance + depositAmount; // add it to the balance 
+            }
+
+            else if (depositAmount < 0.0m) // if the depositAmount is a negative number.
+            {
+                // Display that no action is taken because the user input is less than zero.
+                Console.WriteLine($"--Deposit Amount, {depositAmount}, is less than zero.-- \n No action is taken.\n");
+            }
+
+            else  // if the depositAmount is zero.
+            {
+                // Display that no action is taken because the user input is zero.
+                Console.WriteLine($"--Deposit Amount is zero.-- \n No action is taken.\n");
+            }
+        }
+
+        // method that withdaws (subtracts) only a valid amount from the balance
+        public void Debit(decimal withdrawalAmount)
+        {
+            if (withdrawalAmount > Balance) // if withdrawal amount exceed the Account Balance
+            {
+                Console.WriteLine($"--Debit Amount, {withdrawalAmount}, exceeds account balance.-- \n No action is taken.\n");
+            }
+
+            else if (withdrawalAmount < 0) // if withdrawal amount is a negative number.
+            {
+                Console.WriteLine($"--Debit Amount, {withdrawalAmount}, is less than zero.-- \n No action is taken.\n");
+            }
+
+            else if (withdrawalAmount == 0) // if withdrawal amount is zero.
+            {
+                Console.WriteLine($"--Debit Amount is zero.-- \n No action is taken.\n");
+            }
+
+            else // if the withdrawal amount is a legal value (greater than zero and does not exeed the account balance).
+            {
+                Balance = Balance - withdrawalAmount; // subtract the account balance by the given withdrawal ammount.
+            }
+        }
+    }
+}
---
+++ BankAccounts/AccountTest.cs
@@ -0,0 +1,100 @@
+﻿/*
+ CISP 405 Summer 2019 Tony Wang SID # 1443145
+ 
+ Assignment 2 Description:
+ Modify two given C# files (Account.cs and AccountTest.cs)
+ for learning how to code in C# language.
+ 
+
+ AccountTest.cs
+ The main C# file to create the main program of this assignment,
+ create two accounts from the Account class (Account.cs) and
+ prompt the user to make a deposit and a withdrawal for both
+ account1 and account2.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    class AccountTest
+    {
+        static void Main()
+        {
+            Account account1 = new Account("Jane Green", 50.00m);
+            Account account2 = new Account("John Blue", -7.53m);
+
+            // display initial balance of each object
+            Console.WriteLine("  $$$  Display information of Accounts  $$$");
+            Console.WriteLine(
+               $"account1: {account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine(
+               $"account2: {account2.Name}'s balance: {account2.Balance:C}");
+
+            // deposit prompt for account1 then read input
+            Console.Write("\nEnter deposit amount for account1: ");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(
+               $"adding {depositAmount:C} to account1 balance\n");
+            account1.Deposit(depositAmount); // add to account1's balance
+
+            // display balances
+            Console.WriteLine("  $$$  Display information of Accounts  $$$");
+            Console.WriteLine(
+               $"account1: {account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine(
+               $"account2: {account2.Name}'s balance: {account2.Balance:C}");
+
+            // deposit prompt for account2 then read input
+            Console.Write("\nEnter deposit amount for account2: ");
+            depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(
+               $"adding {depositAmount:C} to account2 balance\n");
+            account2.Deposit(depositAmount); // add to account2's balance
+
+            // display balances
+            Console.WriteLine("  $$$  Display information of Accounts  $$$");
+            Console.WriteLine(
+               $"account1: {account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine(
+               $"account2: {account2.Name}'s balance: {account2.Balance:C}");
+
+            // debit prompt for account1 then read input
+            Console.Write("\nEnter withdrawal amount for account1: ");
+            decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(
+               $"subtracting {withdrawalAmount:C} from account1 balance\n");
+            account1.Debit(withdrawalAmount); // subtract to account1's balance
+
+            // display balances
+            Console.WriteLine("  $$$  Display information of Accounts  $$$");
+            Console.WriteLine(
+               $"account1: {account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine(
+               $"account2: {account2.Name}'s balance: {account2.Balance:C}");
+
+            // debit prompt for account2 then read input
+            Console.Write("\nEnter withdrawal amount for account2: ");
+            withdrawalAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(
+               $"subtracting {withdrawalAmount:C} from account2 balance\n");
+            account2.Debit(withdrawalAmount); // subtract to account2's balance
+
+            // display balances
+            Console.WriteLine("  $$$  Display information of Accounts  $$$");
+            Console.WriteLine(
+               $"account1: {account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine(
+               $"account2: {account2.Name}'s balance: {account2.Balance:C}");
+
+            /* This output line is used as a breakpoint for debug,
+               enable this code line for help to see 
+               the last balance result.
+             */
+            //Console.WriteLine("This is a breakpoint.");
+        }
+    }
+}
